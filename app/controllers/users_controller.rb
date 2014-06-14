f2e2cc2f52f7eaa62fb77f3eb776b13cc572bb99
Charles F. Munat <charles.munat@gmail.com>
2014-06-14 14:45:28 +0800

    Minor refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :get_user_or_redirect,
+    only: [ :show, :edit, :update, :destroy ]
 
   def index
     @users = User.all.entries
   end
 
   def show
-    @user = User.find params[:id]
   end
 
   def new
@@ -23,12 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find params[:id]
   end
 
   def update
-    @user = User.find params[:id]
-
     if @user.update_attributes( user_params )
       redirect_to user_url(@user)
     else
@@ -37,12 +35,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find params[:id]
     @user.destroy
     redirect_to users_url
   end
 
   protected
+
+  def get_user_or_redirect
+    redirect_to users_url unless @user = User.find_by( id: params[:id] )
+  end
 
   def user_params
     params.require(:user).permit(:name, :email)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :email
   field :name
   field :is_admin, type: Boolean
+
+  validates :email, presence: true
 
 end
